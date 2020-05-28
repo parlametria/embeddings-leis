7ede276ca7bab7413463ac7ff2c047107f815fcc
Semar Augusto <semar@most.com.br>
2020-05-28 10:59:58 -0300

    Add print statements at the end to tell how many files were ignored due to being already downloaded, how many files were actually downloaded and how many invalid URL`s there is on the dataset. Some cleaning up on the code was done as well.

--- scripts/download_pdfs.py
+++ scripts/download_pdfs.py
@@ -5,35 +5,42 @@ import pandas as pd
 import numpy as np
 import argparse
 from pathlib import Path
+
 parser = argparse.ArgumentParser(description='Download propositions')
 parser.add_argument('--start', help='Date to start downloads from')
 parser.add_argument('--end', help='Date to end downloads')
 parser.add_argument("--out_dir", required=True,
                     help='Directory to put downloaded pdfs')
-
 args = parser.parse_args()
 
 start = args.start
 end = args.end
 out_dir = args.out_dir
 df = pd.read_csv("data/proposicoes-2020.csv", sep=';')
-df = df.dropna(subset=['urlInteiroTeor'])
-df = df[df['siglaTipo'].isin(['PEC','PL','PLP','MPV','PLV','PDL','PRC'])]
-
-print(df.keys())
-print(df.head())
+# df = df.dropna(subset=['urlInteiroTeor'])
 df = df[df['ultimoStatus_dataHora'] > start]
 df = df[df['ultimoStatus_dataHora'] < end]
 
-print(df.head())
+df = df[df['siglaTipo'].isin(['PEC','PL','PLP','MPV','PLV','PDL','PRC'])]
+
+files_downloaded = 0
+files_in_directory = 0
+invalid_urls = 0
 for i, line in df.iterrows():
-    print(line)
     id_prop = line['id']
-    url = line['urlInteiroTeor']
-    print(url)
-    if url is np.nan:
+    if f'{id_prop}.pdf' in os.listdir(out_dir):
+        files_in_directory += 1
         continue
+    url = line['urlInteiroTeor']
+    if url is np.nan:
+        invalid_urls += 1
+        continue
+
     response = requests.get(url)
     with open(f'{out_dir}/{id_prop}.pdf', 'wb') as f:
         f.write(response.content)
+        files_downloaded += 1
 
+print(f"Downloaded {files_downloaded} files")
+print(f"There were already {files_in_directory} files downloaded")
+print(f"There were {invalid_urls} invalid URL's on the dataset")
