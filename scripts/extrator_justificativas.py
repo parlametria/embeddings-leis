bff72eaf6c4b690ced68e69551c2c14772b75b09
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2020-06-01 18:42:15 -0300

    Modifica script para atender as requisições da revisão Imprime resultados da extração no stdout para serem redirecionados para aquivo Utiliza argparse para lidar com os parâmetros de entrada Adiciona parâmetro para gerar resultados sem header Adequa regex de identificação de justificativas para ser case insensitive Informa quantidade de arquivos cuja justificativa foi extraída

--- scripts/extrator_justificativas.py
+++ scripts/extrator_justificativas.py
@@ -1,49 +1,74 @@
-import os
 import textract
 import re
-import pandas as pd
-from sys import argv
+import argparse
+from numpy import nan
 from tqdm import tqdm
+from sys import stderr
 
 
-def extract_text(path, files):
-    base = {
-        'arquivo': [],
-        'texto_anterior': [],
-        'justificativa': []
-    }
+NEW_LINE = '\n'
+HEADER = 'arquivo;texto_anterior;justificativa'
+REGEX = r'\s*(justificativa|justificação)\s*'
+
+
+def extract_text(file_name):
+    text = textract.process(file_name)
+    text = text.decode('utf-8')
+    text = text.replace(NEW_LINE, ' ')
+    return text
+
+
+def get_justifications(files, header):
+    justified_files = 0
+    if header:
+        print(HEADER)
     for file_name in tqdm(files):
-        text = textract.process(path + '/' + file_name)
-        text = text.decode('utf-8')
+        text = extract_text(file_name)
+        file_name = file_name.split('/')[-1]
         previous, justification = split_text(text)
-        base['arquivo'].append(file_name)
-        base['texto_anterior'].append(previous)
-        base['justificativa'].append(justification)
-    return base
+        justified_files += 0 if justification is nan else 1
+        print(f'{file_name};"{previous}";"{justification}"')
+    print_console(
+        f'{justified_files} arquivos tiveram justificativa encontrada.'
+    )
 
 
 def split_text(text):
-    pattern = r'JUSTIFICATIVA|JUSTIFICAÇÃO|Justificativa'
+    pattern = re.compile(REGEX, flags=re.I)
     parts = re.split(pattern, text)
-    result = (parts[0], parts[1]) if len(parts) > 1 else (parts[0], 'N/A')
+    result = (parts[0], parts[2]) if len(parts) > 1 else (parts[0], nan)
     return result
 
 
-def create_csv(base, output_filename):
-    df = pd.DataFrame(base)
-    df.to_csv(output_filename + '.csv', index=False)
+def print_console(message):
+    output = message + NEW_LINE
+    stderr.write(output)
+
+
+def handle_args():
+    parser = argparse.ArgumentParser(
+        description='Extrai as justificativas dos PDFs de proposições'
+    )
+    parser.add_argument(
+        '--files', dest='files', nargs='*',
+        required="True",
+        help='Lista de arquivos'
+    )
+    parser.add_argument(
+        '--no-header', dest="no_header",
+        action='store_true',
+        help='Não incluir cabeçalho no arquivo resultante'
+    )
+    args = parser.parse_args()
+    return args
 
 
 def main():
-    args = argv[1:]
-    path = args[0]
-    output_filename = args[1]
-    files = os.listdir(path)
-    print(f'Foram encontrados {len(files)} arquivos')
-    print('Extraindo textos')
-    base = extract_text(path, files)
-    create_csv(base, output_filename)
-    print(f'Pronto! O arquivo {output_filename}.csv foi criado')
+    args = handle_args()
+    files = args.files
+    header = not args.no_header
+    print_console('Extraindo justificativas.')
+    get_justifications(files, header)
 
 
 if __name__ == "__main__":
