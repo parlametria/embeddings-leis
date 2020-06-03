b6202a9529dbf721f30a47855a1ed58469d7a9f8
Semar Augusto <semar@most.com.br>
2020-06-03 15:12:44 -0300

    Change naming of files and refactoring

--- scripts/download_pdfs.py
+++ scripts/download_pdfs.py
@@ -1,48 +1,74 @@
 import os
 import sys
+import time
 import requests
 import pandas as pd
 import numpy as np
 import argparse
 from pathlib import Path
 
-parser = argparse.ArgumentParser(description='Download propositions')
-parser.add_argument('--start', help='Date to start downloads from')
-parser.add_argument('--end', help='Date to end downloads')
-parser.add_argument("--out_dir", required=True,
-                    help='Directory to put downloaded pdfs')
-args = parser.parse_args()
+def handle_args():
+    parser = argparse.ArgumentParser(description='Download propositions')
+    parser.add_argument('--start', help='Date to start downloads from')
+    parser.add_argument('--end', help='Date to end downloads')
+    parser.add_argument("--out_dir", required=True,
+                        help='Directory to put downloaded pdfs')
+    return parser.parse_args()
 
-start = args.start
-end = args.end
-out_dir = args.out_dir
-df = pd.read_csv("data/proposicoes-2020.csv", sep=';')
-# df = df.dropna(subset=['urlInteiroTeor'])
-df = df[df['ultimoStatus_dataHora'] > start]
-df = df[df['ultimoStatus_dataHora'] < end]
+def subset_dataset(df, start, end):
+    df = df[df['ultimoStatus_dataHora'] > start]
+    df = df[df['ultimoStatus_dataHora'] < end]
 
-df = df[df['siglaTipo'].isin(['PEC', 'PL', 'PLP',
-                              'MPV','PLV','PDL',
-                              'PRC', 'REQ', 'RIC'])]
+    df = df[df['siglaTipo'].isin(['PEC', 'PL', 'PLP',
+                                  'MPV', 'PLV', 'PDL',
+                                  'PRC', 'REQ', 'RIC'])]
+    return df
 
-files_downloaded = 0
-files_in_directory = 0
-invalid_urls = 0
-for i, line in df.iterrows():
-    id_prop = line['id']
-    if f'{id_prop}.pdf' in os.listdir(out_dir):
-        files_in_directory += 1
-        continue
-    url = line['urlInteiroTeor']
-    if url is np.nan:
-        invalid_urls += 1
-        continue
+def main():
+    args = handle_args()
+    start = args.start
+    end = args.end
+    out_dir = args.out_dir
+    already_downloaded = set(os.listdir(out_dir))
+    df = pd.read_csv("data/propositions.csv", sep=';')
+    df = subset_dataset(df, start, end)
+    # df = df.dropna(subset=['urlInteiroTeor'])
 
-    response = requests.get(url)
-    with open(f'{out_dir}/{id_prop}.pdf', 'wb') as f:
-        f.write(response.content)
-        files_downloaded += 1
+    files_downloaded = 0
+    files_in_directory = 0
+    invalid_urls = 0
+    conn_error = []
+    for i, line in df.iterrows():
+        id_prop = line['id']
+        proposal_num = line['numero']
+        proposal_type = line['siglaTipo']
+        url = line['urlInteiroTeor']
+        if i % 1000 == 0:
+            print(f'{i} files downloaded')
 
-print(f"Downloaded {files_downloaded} files")
-print(f"There were already {files_in_directory} files downloaded")
-print(f"There were {invalid_urls} invalid URL's on the dataset")
+        if f'{id_prop}_{proposal_num}_{proposal_type}.pdf' in already_downloaded:
+            files_in_directory += 1
+            continue
+
+        if url is np.nan:
+            invalid_urls += 1
+            continue
+
+        try:
+            response = requests.get(url)
+        except requests.exceptions.ConnectionError:
+            conn_error.append(url)
+
+        with open(f'{out_dir}/{id_prop}_{proposal_num}_{proposal_type}.pdf', 'wb') as f:
+            f.write(response.content)
+            files_downloaded += 1
+            already_downloaded.add(f'{id_prop}_{proposal_num}_{proposal_type}.pdf')
+        # time.sleep(0.5)
+
+    print(f'Connection Error on these files: {conn_error}')
+    print(f"Downloaded {files_downloaded} files")
+    print(f"There were already {files_in_directory} files downloaded")
+    print(f"There were {invalid_urls} invalid URL's on the dataset")
+
+if __name__=="__main__":
+    main()
