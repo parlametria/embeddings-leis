1f4e6938ec83a94b1fda409444bcea6947ef16b3
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2020-06-08 16:54:08 -0300

    corrige bugs ja identificados no script extrator Muda delimitador do csv gerado Captura exceção lançada pelo textract quando não é possível extrair o texto do arquivo Trata como missing value a string vazia da extracao de scans Melhora função de imprimir no console

--- scripts/extrator_justificativas.py
+++ scripts/extrator_justificativas.py
@@ -8,7 +8,7 @@ from sys import stderr
 
 
 NEW_LINE = '\n'
-HEADER = 'arquivo;id;numero;tipo;texto_anterior;justificativa'
+HEADER = 'arquivo|id|numero|tipo|"texto_anterior"|"justificativa"'
 REGEX = r'\s*(justificativa|justificação)\s*'
 
 
@@ -16,6 +16,7 @@ def extract_text(file_name):
     text = textract.process(file_name)
     text = text.decode('utf-8')
     text = text.replace(NEW_LINE, ' ')
+    text = text.strip()
     return text
 
 
@@ -30,14 +31,19 @@ def get_justifications(path, header):
         print(HEADER)
     for file_name in tqdm(os.listdir(path)):
         file_path = os.path.join(path, file_name)
-        text = extract_text(file_path)
-        file_name = file_name.split('/')[-1]
-        id, numero, tipo = handle_file_name(file_name)
-        previous, justification = split_text(text)
-        justified_files += 0 if justification is nan else 1
-        print(
-            f'{file_name};{id};{numero};{tipo};"{previous}";"{justification}"'
-        )
+        try:
+            file_name = file_name.split('/')[-1]
+            id, numero, tipo = handle_file_name(file_name)
+            text = extract_text(file_path)
+            previous, justification = split_text(text)
+            justified_files += 0 if justification is nan else 1
+            print(
+                f'{file_name}|{id}|{numero}|{tipo}|"{previous}"|"{justification}"'
+            )
+        except textract.exceptions.ShellError:
+            print(
+                f'{file_name}|{id}|{numero}|{tipo}|"{nan}"|"{nan}"'
+            )
     print_console(
         f'{justified_files} arquivos tiveram justificativa encontrada.'
     )
@@ -46,13 +52,21 @@ def get_justifications(path, header):
 def split_text(text):
     pattern = re.compile(REGEX, flags=re.I)
     parts = re.split(pattern, text)
-    result = (parts[0], parts[2]) if len(parts) > 1 else (parts[0], nan)
+    if len(parts) > 1 and is_not_empty(parts[0]) and is_not_empty(parts[2]):
+        result = (parts[0], parts[2])
+    elif len(parts) == 1 and is_not_empty(parts[0]):
+        result = (parts[0], nan)
+    else:
+        result = (nan, nan)
     return result
 
 
-def print_console(message):
-    output = message + NEW_LINE
-    stderr.write(output)
+def is_not_empty(text):
+    return bool(text and text.strip())
+
+
+def print_console(*args, **kwargs):
+    print(*args, file=stderr, **kwargs)
 
 
 def handle_args():
